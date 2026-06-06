3d0093dd1b7f87c0b2f2325183b750773dacf215
Zaid Hasan <numdi007@gmail.com>
2026-06-06 20:06:15 +0530

    cors problem solved

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,8 +21,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
-  credentials: true,
+  origin: [
+    'http://localhost:3000',
+    'https://clubmanchh.onrender.com'
+  ],
+  credentials: true
 }));
 app.use(express.json());
 
